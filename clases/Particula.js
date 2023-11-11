3c05597554ad325d10be8f20415d28bdece6c58a
daniel <daniel.guevara@ucuenca.edu.ec>
2023-11-11 15:53:26 -0500

    se corrigió el sistema de partículas

--- clases/Particula.js
+++ clases/Particula.js
@@ -1,30 +1,34 @@
 class Particula {
-  constructor(_x, _y) {
+  constructor(_x, _y, _color) {
     this.pos = createVector(_x, _y);
-    this.lifespan = Math.ceil(random(10, 200));
     this.isAlive = true;
-    this.side = Math.ceil(random(3, 15));
-    this.color = random(150);
+    this.side = random(3, 15);
+    this.color = _color || color(random(255), random(255), random(255));
+    this.lifespan = Math.ceil(random(10, 200));
   }
-  update() {
-    this.r = random(100);
 
-    if (this.r < 20) {
+  update() {
+    let r = random(100);
+
+    if (r < 20 && this.pos.y > height / 4) {
       this.pos.y -= this.side;
-    } else if (this.r < 40) {
+    } else if (r < 40 && this.pos.x < (3 * width) / 4) {
       this.pos.x += this.side;
-    } else if (this.r < 80) {
+    } else if (r < 80 && this.pos.y < (3 * height) / 4) {
       this.pos.y += this.side;
-    } else {
+    } else if (this.pos.x > width / 4) {
       this.pos.x -= this.side;
     }
+
     this.lifespan -= 1;
     if (this.lifespan <= 0) {
       this.isAlive = false;
     }
   }
+
   display() {
-    Fill(this.color);
+    console.log(this.color);
+    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2]);
     stroke(255);
     square(this.pos.x, this.pos.y, this.side);
   }
--- clases/Sistema.js
+++ clases/Sistema.js
@@ -1,37 +1,52 @@
-class Particula {
+class Sistema {
   constructor(_x, _y, _color) {
     this.pos = createVector(_x, _y);
-    this.lifespan = Math.ceil(random(10, 100));
     this.isAlive = true;
-    this.side = Math.ceil(random(3, 15));
+    this.side = random(3, 15);
     this.color = _color;
+    this.lifespan = Math.ceil(random(10, 200));
   }
-  update() {
-    this.r = random(100);
 
-    if (this.r < 20) {
-      // hacia arriba
+  update() {
+    let r = random(100);
+
+    if (r < 20) {
       this.pos.y -= this.side;
-    } else if (this.r < 40) {
-      // hacia la derecha
+    } else if (r < 40) {
       this.pos.x += this.side;
-    } else if (this.r < 80) {
-      // hacia abajo
+    } else if (r < 80) {
       this.pos.y += this.side;
     } else {
-      // hacia izquierda
       this.pos.x -= this.side;
     }
 
     this.lifespan -= 1;
-
     if (this.lifespan <= 0) {
       this.isAlive = false;
     }
   }
+
   display() {
-    fill(this.color);
+    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2]);
     stroke(255);
     square(this.pos.x, this.pos.y, this.side);
+  }
+}
+
+let sistemas = [];
+
+function setup() {
+  createCanvas(400, 400);
+  for (let i = 0; i < 5; i++) {
+    let colorSistema = color(random(255), random(255), random(255));
+    sistemas.push(new Sistema(random(width), random(height), colorSistema));
+  }
+}
+
+function draw() {
+  background(220);
+  for (let i = 0; i < sistemas.length; i++) {
+    sistemas[i].update();
+    sistemas[i].display();
   }
 }
